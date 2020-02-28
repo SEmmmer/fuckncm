782920e81385e1c9a60d1e031070315668f805e7
Shirakami Emmmer <yjhdandan@163.com>
2020-02-28 19:16:31 +0800

    little update: unfixed bug

--- main.c
+++ main.c
@@ -1,34 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "myfun.h"
 //#include "/usr/local/Cellar/openssl/1.0.2t/include/openssl/aes.h"
 //just for fun, LOL
 #include <openssl/aes.h>
-#include <ctype.h>
 
 int main() {
 
-    const char ncmFile[] = "CTENFDAM";
+    const unsigned char ncmFile[] = "CTENFDAM";
 
-    char firstKey[] = {0x68, 0x7A, 0x48, 0x52, 0x41, 0x6D, 0x73, 0x6F, 0x35, 0x6B, 0x49, 0x6E, 0x62, 0x61, 0x78, 0x57};
-    char secondKey[] = {0x23, 0x31, 0x34, 0x6C, 0x6A, 0x6B, 0x5F, 0x21, 0x5C, 0x5D, 0x26, 0x30, 0x55, 0x3C, 0x27, 0x28};
+    const unsigned char firstKey[] = {0x68, 0x7A, 0x48, 0x52, 0x41, 0x6D, 0x73, 0x6F,
+                                      0x35, 0x6B, 0x49, 0x6E, 0x62, 0x61, 0x78, 0x57};
+    const unsigned char secondKey[] = {0x23, 0x31, 0x34, 0x6C, 0x6A, 0x6B, 0x5F, 0x21,
+                                       0x5C, 0x5D, 0x26, 0x30, 0x55, 0x3C, 0x27, 0x28};
+//
+//    FILE *aSong;
+//    aSong = fopen("tmp/test.ncm", "rb");
+//
+//    unsigned char buffer[8];
+//    fread(buffer, 8, 1, aSong);
+//    if (memcmp(buffer, ncmFile, 8) != 0) { exit(1); }//不是ncm
+//
+//    int keyLength = 0;
+//    fseek(aSong, 10, SEEK_SET);
+//    fread(&keyLength, 4, 1, aSong);
 
-    FILE *aSong;
-    aSong = fopen("tmp/test.ncm", "r");
+//    unsigned char *keyData = (unsigned char *) malloc(keyLength);
+//    fread(keyData, keyLength, 1, aSong);
+    unsigned char Data[] = "Segmentation occurs while calling these two function with key-size=192 and 256";
 
-    char buffer[8];
-    fread(buffer, 8, 1, aSong);
-    if (strcmp(buffer, ncmFile) != 0) {}//不是ncm
+    printf("%s\n", Data);
+    AES_KEY encode;
+    private_AES_set_encrypt_key(firstKey, 128, &encode);
+    AES_KEY decode;
+    private_AES_set_decrypt_key(firstKey, 128, &decode);
 
-    int keyLength = 0;
-    fseek(aSong, 10, SEEK_SET);
-    fread(&keyLength, 4, 1, aSong);
-
-    char *keyData = (char *) malloc(keyLength);
-    fread(keyData, keyLength, 1, aSong);
-
-    printf("%s", keyData);
-//    int data = private_AES_set_decrypt_key()
+    unsigned char massage1[4];
+    unsigned char massage2[4];
+    printf("---AES Decryption started----\n");
+    AES_ecb_encrypt(Data, massage1, &encode, AES_ENCRYPT);
+    printf("%s\n", massage1);
+    AES_ecb_encrypt(massage1, massage2, &decode, AES_DECRYPT);
+    printf("%s\n", massage2);
+    printf("---AES Decryption finished---\n");
     return 0;
 }
