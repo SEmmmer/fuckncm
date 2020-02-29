877fe8a725ba34253e3d86e51b1ca6bfbf156e24
Shirakami Emmmer <yjhdandan@163.com>
2020-02-29 22:11:49 +0800

    now we can get the keyData

--- main.c
+++ main.c
@@ -14,41 +14,45 @@ int main() {
                                       0x35, 0x6B, 0x49, 0x6E, 0x62, 0x61, 0x78, 0x57};
     const unsigned char secondKey[] = {0x23, 0x31, 0x34, 0x6C, 0x6A, 0x6B, 0x5F, 0x21,
                                        0x5C, 0x5D, 0x26, 0x30, 0x55, 0x3C, 0x27, 0x28};
-//
-//    FILE *aSong;
-//    aSong = fopen("tmp/test.ncm", "rb");
-//
-//    unsigned char buffer[8];
-//    fread(buffer, 8, 1, aSong);
-//    if (memcmp(buffer, ncmFile, 8) != 0) { exit(1); }//不是ncm
-//
-//    int keyLength = 0;
-//    fseek(aSong, 10, SEEK_SET);
-//    fread(&keyLength, 4, 1, aSong);
 
-//    unsigned char *keyData = (unsigned char *) malloc(keyLength);
-//    fread(keyData, keyLength, 1, aSong);
-    unsigned char Data[] = "Segmentation occurs while calling these two function with key-size=192 and 256";
+    FILE *aSong;
+    aSong = fopen("tmp/test.ncm", "rb");
 
-    printf("%s\n", Data);
+    unsigned char buffer[8];
+    fread(buffer, 8, 1, aSong);
+    if (memcmp(buffer, ncmFile, 8) != 0) { exit(1); }//不是ncm
+
+    int keyLength = 0;
+    fseek(aSong, 10, SEEK_SET);
+    fread(&keyLength, 4, 1, aSong);
+
+    unsigned char *keyData = (unsigned char *) malloc(keyLength);
+    fread(keyData, keyLength, 1, aSong);
+    printf("%d\n", keyLength);
+    printf("%s\n", keyData);
+
+    for (int i = 0; i < keyLength; ++i) {
+        keyData[i] ^= 0x64;
+        printf(" %x", keyData[i]);
+    }
+//    unsigned char Data[128] = "Segmentation occurs while calling these two function with key-size=192 and 256";
+//    printf("%s\n", Data);
     AES_KEY encode;
     private_AES_set_encrypt_key(firstKey, 128, &encode);
     AES_KEY decode;
     private_AES_set_decrypt_key(firstKey, 128, &decode);
+    unsigned char massage[128] = {0};
 
-    unsigned char massage1[128];
-    unsigned char massage2[128];
-
-    printf("---AES Decryption started----\n");
+    printf("\n---AES Decryption started----\n");
     for (int j = 0; j < 8; ++j) {
-        AES_ecb_encrypt(Data + j * 16, massage1 + j * 16, &encode, AES_ENCRYPT);
+        AES_ecb_encrypt(keyData + 16 * j, massage + 16 * j, &decode, AES_DECRYPT);
     }
-    printf("密文：%s\n", massage1);
-    for (int i = 0; i < 8; ++i) {
-        AES_ecb_encrypt(massage1 + i * 16, massage2 + i * 16, &decode, AES_DECRYPT);
+    printf("%.128s\n", massage);
+    for (int k = 17; k < 128 ; ++k) {
+//        printf("%c",massage[k]);
+        putchar(massage[k]);
     }
-
-    printf("解密后结果：%s\n", massage2);
-    printf("---AES Decryption finished---\n");
+    printf("\n---AES Decryption finished---\n");
+    free(keyData);
     return 0;
 }
