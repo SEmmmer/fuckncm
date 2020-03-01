c30082d64f18b22fa41e28bc4137ef169752d5f3
Shirakami Emmmer <yjhdandan@163.com>
2020-03-01 19:26:11 +0800

    try to fix
    Undefined symbols for architecture x86_64:
      "_base64_encode", referenced from:
          _main in test.c.o
    ld: symbol(s) not found for architecture x86_64
    clang: error: linker command failed with exit code 1 (use -v to see invocation)
    make[3]: *** [test] Error 1
    make[2]: *** [CMakeFiles/test.dir/all] Error 2
    make[1]: *** [CMakeFiles/test.dir/rule] Error 2
    make: *** [test] Error 2

--- test.c
+++ test.c
@@ -2,20 +2,19 @@
 // Created by Shirakami Emmmer on 2020/3/1.
 //
 #include <stdio.h>
-#include "/usr/local/Cellar/base64/include/libbase64.h"
+#include "base64.h"
 
 int main(int argc, char *argv[]) {
 
-    char src[] = "hello world123456789";
+    unsigned char src[] = "hello world123456789";
     char massage[128] = {0};
     size_t src_len = sizeof(src) - 1;
     size_t out_len;
 
     printf("%zd\n", sizeof(src));
-    base64_encode(src, sizeof(src) - 1, massage, &out_len, 0);
-
-//    base64_decode();
+    base64_encode(src, 128, massage);
     printf("%s\n", src);
+    printf("%s\n", massage);
 
     return 0;
 }
