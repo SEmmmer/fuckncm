d7a9d3568bbe1af02a642ffe67572fb2c3295787
Shirakami Emmmer <yjhdandan@163.com>
2020-03-02 22:34:14 +0800

    creat a rc4

--- test.c
+++ test.c
@@ -5,12 +5,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "base64.h"
+#include "WjCryptLib_Rc4.h"
 
 int main() {
-    unsigned char src[20] = "hello world";
-    char *src1 = src;
-    char massage[20];
-    printf("%s\n", src);
-    base64_encode(src, 20, massage);
-    printf("%s\n", massage);
+//    key_data = bytearray(key_data)
+//    key_box = bytearray(range(256))
+//    c = 0
+//    last_byte = 0
+//    key_offset = 0
+//    for i in range(256):
+//    swap = key_box[i]
+//    c = (swap + last_byte + key_data[key_offset]) & 0xff
+//    key_offset += 1
+//    if key_offset >= key_length: key_offset = 0
+//    key_box[i] = key_box[c]
+//    key_box[c] = swap
+//    last_byte = c
+    return 0;
 }
