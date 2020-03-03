aa3fbc56acc5506b922adaba134ff4f839c057eb
Shirakami Emmmer <yjhdandan@163.com>
2020-03-03 22:11:10 +0800

    creat a rc4

--- test.c
+++ test.c
@@ -10,9 +10,20 @@
 int main() {
 //    key_data = bytearray(key_data)
 //    key_box = bytearray(range(256))
+    unsigned char key_data[128] = {0};
+    unsigned char key_box[256] = {0};
 //    c = 0
 //    last_byte = 0
 //    key_offset = 0
+    int c = 0;
+    int last_byte = 0;
+    int key_offset = 0;
+    int swap = 0;
+
+    for (int i = 0; i < 256; ++i) {
+        swap = key_box[i];
+        c = (swap + last_byte + key_data[key_offset]) & 0xff;
+    }
 //    for i in range(256):
 //    swap = key_box[i]
 //    c = (swap + last_byte + key_data[key_offset]) & 0xff
