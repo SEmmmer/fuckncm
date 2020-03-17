ce8f946fc2d86847652f2936d6b0b348ea87d246
Shirakami Emmmer <yjhdandan@163.com>
2020-03-17 22:59:20 +0800

    e all fun s

--- myfun.h
+++ myfun.h
@@ -5,20 +5,54 @@
 #ifndef FUCKNCM_MYFUN_H
 #define FUCKNCM_MYFUN_H
 
-//十六进制字符转二进制字符
-void Print_hex_to_Bin(char c) {
-    char x[17][5] =
-            {
-                    "0000", "0001", "0010", "0011",
-                    "0100", "0101", "0110", "0111",
-                    "1000", "1001", "1010", "1011",
-                    "1100", "1101", "1110", "1111",
-            };
-    int n = 16;
-    if (c >= 'a' && c <= 'f') n = c - 'a' + 10;
-    if (c >= 'A' && c <= 'F') n = c - 'A' + 10;
-    if (c >= '0' && c <= '9') n = c - '0';
-    printf("%s", x[n]);
+
+
+void AES_Decryption(const unsigned char *data, const unsigned char *inKey, size_t size, unsigned char *outData) {
+    AES_KEY key;
+    private_AES_set_decrypt_key(inKey, 128, &key);
+    unsigned char message[size];
+
+    for (int j = 0; j < size / 16; ++j) { AES_ecb_encrypt(data + 16 * j, message + 16 * j, &key, AES_DECRYPT); }
+    for (int i = 0; i < size; ++i) { *(outData + i) = message[i]; }
+}
+
+void RC4_Decryption(const unsigned char *rc4Key, size_t size, unsigned char *keyBox) {
+    for (int j = 0; j < 256; ++j) { keyBox[j] = j; }
+
+    int keyOffset = 0;
+    unsigned char c = 0x00;
+    unsigned char swap = 0x00;
+    unsigned char lastByte = 0x00;
+
+    for (int i = 0; i < 256; ++i) {
+        swap = keyBox[i];
+        c = (swap + lastByte + rc4Key[keyOffset]) & 0xff;
+        keyOffset += 1;
+        if (keyOffset >= size) { keyOffset = 0; }
+        keyBox[i] = keyBox[c];
+        keyBox[c] = swap;
+        lastByte = c;
+    }
+}
+
+void audioDecoding(FILE *ncmFile, FILE *outFile, const unsigned char *keyBox) {
+    long position = ftell(ncmFile);
+    fseek(ncmFile, 0, SEEK_END);
+    long fileSize = ftell(ncmFile);
+    fseek(ncmFile, position, SEEK_SET);
+
+    int j = 0;
+    unsigned char chunk[0x8000] = {0};
+    int chunkLength = sizeof(chunk);
+
+    for (long loop = 0; loop <= (fileSize - position) / chunkLength; loop++) {
+        fread(chunk, chunkLength, 1, ncmFile);
+        for (int i = 1; i < chunkLength + 1; ++i) {
+            j = i & 0xff;
+            chunk[i - 1] ^= keyBox[(keyBox[j] + keyBox[(keyBox[j] + j) & 0xff]) & 0xff];
+        }
+        fwrite(chunk, chunkLength, 1, outFile);
+    }
 }
 
 #endif //FUCKNCM_MYFUN_H
