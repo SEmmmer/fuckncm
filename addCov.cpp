21aeaed2fe667cbf71059eebaaccf2f5bf5e6c5f
Shirakami Emmmer <yjhdandan@163.com>
2020-03-22 23:02:35 +0800

    i taglib s

--- addCov.cpp
+++ addCov.cpp
@@ -1,11 +1,43 @@
 //
 // Created by Shirakami Emmmer on 2020/3/22.
 //
+#define Type TagLib::String::UTF8
 
 #include <iostream>
-#include <taglib/id3v2tag.h>
+#include <string>
+#include <json-c/json.h>
+#include <taglib/taglib.h>
+#include <taglib/fileref.h>
+#include <taglib/tstring.h>
 
-int main(){
-    std::cout << "Hello world" << std::endl;
+void readJson() {
+
+}
+
+void setTag() {
+
+}
+
+void buffer(){
+    std::string title = "中文测试";
+    const std::string &s = title;
+    TagLib::String give_me_a_name(s, Type);
+
+    TagLib::FileRef f("tmp/nocov.mp3");
+    f.tag()->setArtist(give_me_a_name);
+    f.tag()->setAlbum(s);
+    f.tag()->setTitle(s);
+    f.tag()->setComment(title);
+    f.tag()->setGenre(title);
+    f.tag()->setTrack(1);
+    f.tag()->setYear(1);
+    f.save();
+}
+
+int main() {
+
+
+
+
     return 0;
 }
--- test.c
+++ test.c
@@ -2,32 +2,28 @@
 #include <json-c/json.h>
 #include "test.h"
 
+
 int main(int argc, char *argv[]) {
 
     struct json_object *im = NULL;
     im = json_object_from_file("tmp/tmp.json");
     printf("%s\n", json_object_to_json_string(im));
-    struct json_object *result_object = NULL;
-    result_object = json_object_object_get(im, "musicId");
-    printf("%s\n", json_object_to_json_string(result_object));;
+    struct json_object *result = NULL;
 
-    result_object = json_object_object_get(im, "musicName");
-    printf("%s\n", json_object_to_json_string(result_object));
+    result = json_object_object_get(im, "musicName");
+    printf("%s\n", json_object_to_json_string(result));
 
-    result_object = json_object_object_get(im, "artist");
-    printf("%s\n", json_object_to_json_string(result_object));
+    result = json_object_object_get(im, "artist");
+    printf("%s\n", json_object_to_json_string(result));
 
-    result_object = json_object_object_get(im, "album");
-    printf("%s\n", json_object_to_json_string(result_object));
+    result = json_object_object_get(im, "album");
+    printf("%s\n", json_object_to_json_string(result));
 
-    result_object = json_object_object_get(im, "albumPic");
-    printf("%s\n", json_object_to_json_string(result_object));
+    result = json_object_object_get(im, "bitrate");
+    printf("%s\n", json_object_to_json_string(result));
 
-    result_object = json_object_object_get(im, "bitrate");
-    printf("%s\n", json_object_to_json_string(result_object));
-
-    result_object = json_object_object_get(im, "format");
-    printf("%s\n", json_object_to_json_string(result_object));
+    result = json_object_object_get(im, "format");
+    printf("%s\n", json_object_to_json_string(result));
 
 
     return 0;
